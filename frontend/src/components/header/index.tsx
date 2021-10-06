dcfcb97f14e47f04adafd60c4710ec1641cb7883
Wagner Pires <wagnerpires@hotmail.com>
2021-10-06 19:22:07 -0300

    Project clean, Header ready

---
+++ frontend/src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Header() {
+  return (
+    <header className="main-header-container">
+      <h1 className="main-header-title">WR - Tecnologia em Sistemas</h1>
+    </header>
+  );
+}
+
+export default Header;
